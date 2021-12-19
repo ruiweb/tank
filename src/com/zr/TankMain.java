f178f97553487f3094a3b76ffcfbeced1e8fcd29
ruiweb <371556343@qq.com>
2021-12-19 22:18:40 +0800

    坦克大战小游戏demo

--- src/com/zr/TankMain.java
+++ src/com/zr/TankMain.java
@@ -7,7 +7,7 @@ import java.awt.event.WindowEvent;
 public class TankMain {
     public static void main(String[] args) throws InterruptedException {
         TankFrame tf =new TankFrame();
-        //在主线程每隔50ms刷新窗口，调用repaint方法，
+        //在主线程每隔50ms刷新窗口，调用repaint方法，ccc
         while (true){
             Thread.sleep(50);
             tf.repaint();
